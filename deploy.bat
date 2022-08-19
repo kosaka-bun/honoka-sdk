69202453b6f38d97784ad9fbbc64d001f8befcdc
Kosaka Bun <kosaka-bun@qq.com>
2022-08-19 08:28:02 +0800

    项目架构替换为gradle

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,7 @@
 :: maven先部署到本地git仓库
-call mvn deploy -DskipTests -DaltDeploymentRepository=^
-maven.repo::default::file://F:/Projects/OtherProject/RemoteMavenRepository/repository
+call gradlew -PremoteMavenRepositoryUrl=^
+file://F:/Projects/OtherProject/RemoteMavenRepository/repository ^
+publish
 
 :: 进入git仓库，然后提交并推送
 cd /d F:\Projects\OtherProject\RemoteMavenRepository
--- honoka-json/honoka-json-fastjson/src/main/java/de/honoka/sdk/json/fastjson/FastJsonArray.java
+++ honoka-json/honoka-json-fastjson/src/main/java/de/honoka/sdk/json/fastjson/FastJsonArray.java
@@ -8,94 +8,95 @@ import java.util.Iterator;
 
 @SuppressWarnings({ "unchecked" })
 public class FastJsonArray<T> extends JsonArray<T> {
-	
-	JSONArray originalJsonArray;
-	
-	//region init
-	
-	@Override
-	protected void initEmpty() {
-		originalJsonArray = new JSONArray();
-	}
-	
-	@Override
-	protected void initByJsonString(String jsonStr) {
-		originalJsonArray = JSON.parseArray(jsonStr);
-	}
-	
-	@Override
-	protected <A> void initByOriginalJsonArray(A originalJsonArray) {
-		this.originalJsonArray = (JSONArray) originalJsonArray;
-	}
-	
-	public FastJsonArray(Class<?> dataType) {
-		super(dataType);
-		initEmpty();
-	}
-	
-	public FastJsonArray(String jsonStr, Class<?> dataType) {
-		super(dataType);
-		initByJsonString(jsonStr);
-	}
-	
-	FastJsonArray(JSONArray originalJsonArray, Class<?> dataType) {
-		super(dataType);
-		initByOriginalJsonArray(originalJsonArray);
-	}
-	
-	//endregion
-	
-	@Override
-	public T get(int index) {
-		return (T) new FastJsonDataConverter(originalJsonArray.get(index))
-				.transfer(dataType);
-	}
-	
-	@Override
-	public Iterator<T> iterator() {
-		return new Iterator<>() {
-			
-			private final Iterator<Object> originalIterator =
-					originalJsonArray.iterator();
-			
-			@Override
-			public boolean hasNext() {
-				return originalIterator.hasNext();
-			}
-			
-			@Override
-			public T next() {
-				var fjdc = new FastJsonDataConverter(originalIterator.next());
-				if(fjdc.isJsonArray())
-					return (T) fjdc.toJsonArray(dataType);
-				return (T) fjdc.transfer(dataType);
-			}
-		};
-	}
-	
-	@Override
-	public String toString() {
-		return originalJsonArray.toString(Common.serializerFeatures);
-	}
-	
-	@Override
-	public int size() {
-		return originalJsonArray.size();
-	}
-	
-	@Override
-	public boolean add(T t) {
-		try {
-			originalJsonArray.add(Common.toOriginalJsonElement(t));
-			return true;
-		} catch(Throwable th) {
-			return false;
-		}
-	}
-	
-	@Override
-	protected boolean originalRemove(int index) {
-		Object elem = originalJsonArray.remove(index);
-		return elem != null;
-	}
+
+    JSONArray originalJsonArray;
+
+    //region init
+
+    @Override
+    protected void initEmpty() {
+        originalJsonArray = new JSONArray();
+    }
+
+    @Override
+    protected void initByJsonString(String jsonStr) {
+        originalJsonArray = JSON.parseArray(jsonStr);
+    }
+
+    @Override
+    protected <A> void initByOriginalJsonArray(A originalJsonArray) {
+        this.originalJsonArray = (JSONArray) originalJsonArray;
+    }
+
+    public FastJsonArray(Class<?> dataType) {
+        super(dataType);
+        initEmpty();
+    }
+
+    public FastJsonArray(String jsonStr, Class<?> dataType) {
+        super(dataType);
+        initByJsonString(jsonStr);
+    }
+
+    FastJsonArray(JSONArray originalJsonArray, Class<?> dataType) {
+        super(dataType);
+        initByOriginalJsonArray(originalJsonArray);
+    }
+
+    //endregion
+
+    @Override
+    public T get(int index) {
+        return (T) new FastJsonDataConverter(originalJsonArray.get(index))
+                .transfer(dataType);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private final Iterator<Object> originalIterator =
+                    originalJsonArray.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return originalIterator.hasNext();
+            }
+
+            @Override
+            public T next() {
+                FastJsonDataConverter fjdc = new FastJsonDataConverter(
+                        originalIterator.next());
+                if(fjdc.isJsonArray())
+                    return (T) fjdc.toJsonArray(dataType);
+                return (T) fjdc.transfer(dataType);
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        return originalJsonArray.toString(Common.serializerFeatures);
+    }
+
+    @Override
+    public int size() {
+        return originalJsonArray.size();
+    }
+
+    @Override
+    public boolean add(T t) {
+        try {
+            originalJsonArray.add(Common.toOriginalJsonElement(t));
+            return true;
+        } catch(Throwable th) {
+            return false;
+        }
+    }
+
+    @Override
+    protected boolean originalRemove(int index) {
+        Object elem = originalJsonArray.remove(index);
+        return elem != null;
+    }
 }
