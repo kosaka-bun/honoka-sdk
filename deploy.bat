da6defbcd33ef731cca2f5dbb4793f212f22eea5
Kosaka Bun <kosaka-bun@qq.com>
2023-08-14 22:17:26 +0800

    Add gradle_ci.yml

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,11 @@
 :: maven先部署到本地git仓库
 call gradlew -PremoteMavenRepositoryUrl=^
-file://F:/Projects/Other/remote-maven-repo/repository ^
+file://C:/Personal/Projects/Other/remote-maven-repo/repository ^
 publish
 
 :: 进入git仓库，然后提交并推送
-cd /d F:\Projects\Other\remote-maven-repo
+cd /d C:\Personal\Projects\Other\remote-maven-repo
+git pull
 git add . && ^
 git commit -m "Update honoka-sdk" && ^
 git push
