fabc4db11e865b0391e081eb679adb45a610bd5d
Kosaka Bun <kosaka-bun@qq.com>
2024-06-02 20:13:08 +0800

    Gradle checkVersionOfProjects任务支持校验依赖项的版本

--- buildSrc/src/main/java/de/honoka/gradle/buildsrc/MavenPublish.kt
+++ buildSrc/src/main/java/de/honoka/gradle/buildsrc/MavenPublish.kt
@@ -1,6 +1,9 @@
 package de.honoka.gradle.buildsrc
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ConfigurationContainer
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.internal.artifacts.dsl.dependencies.DefaultDependencyHandler
 import org.gradle.api.publish.maven.MavenPom
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.create
@@ -28,22 +31,6 @@ object MavenPublish {
         projectsWillPublish.add(this)
     }
 
-    private fun checkVersionOfProjects() {
-        var passed = true
-        println("Versions:\n")
-        listOf(rootProject, *projectsWillPublish.toTypedArray()).forEach {
-            if(!passed) return@forEach
-            //若project未设置version，则这里取到的version值为unspecified
-            println("${it.name}=${it.version}")
-            passed = it.version.toString().lowercase().run {
-                !(isEmpty() || this == "unspecified" || contains("dev"))
-            }
-        }
-        println("\nResults:\n")
-        println("results.passed=$passed")
-        println()
-    }
-
     fun Project.defineCheckVersionOfProjectsTask() {
         this@MavenPublish.rootProject = rootProject
         tasks.register("checkVersionOfProjects") {
@@ -52,5 +39,37 @@ object MavenPublish {
                 checkVersionOfProjects()
             }
         }
+    }
+
+    private fun checkVersionOfProjects() {
+        var passed = true
+        val dependencies = HashSet<Dependency>()
+        println("Versions:\n")
+        listOf(rootProject, *projectsWillPublish.toTypedArray()).forEach {
+            if(!passed) return@forEach
+            //若project未设置version，则这里取到的version值为unspecified
+            println("${it.name}=${it.version}")
+            dependencies.addAll(it.rawDependencies)
+            passed = it.version.isAvaliableVersion()
+        }
+        if(passed) passed = checkVersionOfDependencies(dependencies)
+        println("\nResults:\n")
+        println("results.passed=$passed")
+        println()
+    }
+
+    private fun Any?.isAvaliableVersion(): Boolean = toString().lowercase().run {
+        !(isEmpty() || this == "unspecified" || contains("dev"))
+    }
+
+    private fun checkVersionOfDependencies(dependencies: Set<Dependency>): Boolean {
+        var passed = true
+        println("\nDependencies:\n")
+        dependencies.forEach {
+            if(!passed) return@forEach
+            println("${it.group}:${it.name}=${it.version}")
+            passed = it.version.isAvaliableVersion()
+        }
+        return passed
     }
 }
--- buildSrc/src/main/java/de/honoka/gradle/buildsrc/Project.kt
+++ buildSrc/src/main/java/de/honoka/gradle/buildsrc/Project.kt
@@ -1,6 +1,9 @@
 package de.honoka.gradle.buildsrc
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ConfigurationContainer
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.internal.artifacts.dsl.dependencies.DefaultDependencyHandler
 import org.gradle.api.internal.catalog.VersionModel
 
 @Suppress("UNCHECKED_CAST")
@@ -18,3 +21,19 @@ fun Project.libVersions(): Map<String, VersionModel> {
 }
 
 fun Map<String, VersionModel>.getVersion(key: String): String = get(key)?.version.toString()
+
+val Project.rawDependencies: Set<Dependency>
+    get() {
+        val configurationContainerField = DefaultDependencyHandler::class.java.getDeclaredField("configurationContainer")
+        val configurationContainer = configurationContainerField.run {
+            isAccessible = true
+            get(dependencies) as ConfigurationContainer
+        }
+        val set = HashSet<Dependency>()
+        configurationContainer.forEach {
+            it.dependencies.forEach {
+                set.add(it as Dependency)
+            }
+        }
+        return set
+    }
