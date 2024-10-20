df682b9dd55f11f9ff32c279b46566c50ec08b7e
Kosaka Bun <kosaka-bun@qq.com>
2024-10-20 21:28:46 +0800

    Update honoka-framework-utils/ApiResponse

--- honoka-framework-utils/src/main/java/de/honoka/sdk/util/framework/web/ApiResponse.java
+++ honoka-framework-utils/src/main/java/de/honoka/sdk/util/framework/web/ApiResponse.java
@@ -8,39 +8,45 @@ import javax.servlet.http.HttpServletResponse;
 @Data
 @Accessors(chain = true)
 public class ApiResponse<T> {
-
+    
     private Integer code;
-
+    
     private Boolean status;
-
+    
     private String msg;
-
+    
     private T data;
-
+    
+    private ApiResponse() {}
+    
+    public static <T1> ApiResponse<T1> of() {
+        return new ApiResponse<>();
+    }
+    
     public static <T1> ApiResponse<T1> success(String msg, T1 data) {
         return new ApiResponse<T1>()
-                .setCode(HttpServletResponse.SC_OK)
-                .setStatus(true)
-                .setMsg(msg)
-                .setData(data);
+            .setCode(HttpServletResponse.SC_OK)
+            .setStatus(true)
+            .setMsg(msg)
+            .setData(data);
     }
-
+    
     public static <T1> ApiResponse<T1> success(T1 data) {
         return success(null, data);
     }
-
+    
     public static ApiResponse<?> success() {
         return success(null);
     }
-
+    
     public static ApiResponse<?> fail(int httpStatus, String msg) {
         return new ApiResponse<>()
-                .setCode(httpStatus)
-                .setStatus(false)
-                .setMsg(msg)
-                .setData(null);
+            .setCode(httpStatus)
+            .setStatus(false)
+            .setMsg(msg)
+            .setData(null);
     }
-
+    
     public static ApiResponse<?> fail(String msg) {
         return fail(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, msg);
     }
