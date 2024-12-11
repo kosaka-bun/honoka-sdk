5edb4797f3db6315a4f80219f60a2f71342e532a
Kosaka Bun <kosaka-bun@qq.com>
2024-12-11 21:22:43 +0800

    Update honoka-kotlin-utils/WebUtils

--- honoka-kotlin-utils/src/main/java/de/honoka/sdk/util/kotlin/text/Strings.kt
+++ honoka-kotlin-utils/src/main/java/de/honoka/sdk/util/kotlin/text/Strings.kt
@@ -1,13 +1,46 @@
 package de.honoka.sdk.util.kotlin.text
 
-fun String?.singleLine(whiteSpaceOnEnd: Boolean = false): String {
+/**
+ * 将多行字符串拼接为单行，支持行边界字符，默认使用“|”字符作为行边界。
+ *
+ * 基本逻辑：
+ * 1. 去掉传入字符串两端的空格。
+ * 2. 将字符串拆分为行。
+ * 3. 遍历每行，先去掉每行两端的空格。
+ * 4. 若某行以“|”开头，则去掉第一个“|”。以“|”结尾，则去掉最后一个“|”。
+ * 5. 将处理好的行拼接到builder中。
+ * 6. 返回builder的值。
+ *
+ * 注意事项：
+ * 1. 本方法仅适合用于Kotlin代码中的多行文本字面量。
+ * 2. 若某行以`${}`开头或结尾，由于不知道表达式最终的值是否会以“|”开头或结尾，为了避免去除
+ *    表达式结果当中包含的“|”，请在以`${}`开头或结尾的行的行首或行尾额外添加一个“|”。
+ */
+fun String?.singleLine(borderChar: Char = '|', whiteSpaceOnEnd: Boolean = false): String {
     val builder = StringBuilder()
     toString().trim().lineSequence().forEach {
         val line = it.trim()
-        val startIndex = if(line.startsWith("|")) 1 else 0
-        val endIndex = if(line.endsWith("|")) line.length - 1 else line.length
+        val startIndex = if(line.startsWith(borderChar)) 1 else 0
+        val endIndex = if(line.endsWith(borderChar)) line.lastIndex else line.length
         if(startIndex >= endIndex) return@forEach
-        builder.append(line.substring(startIndex, endIndex))
+        builder.run {
+            append(line.substring(startIndex, endIndex))
+            if(whiteSpaceOnEnd) append(" ")
+        }
+    }
+    return builder.toString()
+}
+
+/**
+ * 直接将多行字符串的每行去除两端空格后，拼接在一起。
+ */
+fun String?.simpleSingleLine(whiteSpaceOnEnd: Boolean = false): String {
+    val builder = StringBuilder()
+    toString().trim().lineSequence().forEach {
+        builder.run {
+            append(it.trim())
+            if(whiteSpaceOnEnd) append(" ")
+        }
     }
     return builder.toString()
 }
--- honoka-spring-boot-starter/src/main/java/de/honoka/sdk/spring/starter/core/web/WebUtils.kt
+++ honoka-spring-boot-starter/src/main/java/de/honoka/sdk/spring/starter/core/web/WebUtils.kt
@@ -42,4 +42,18 @@ object WebUtils {
     }
     
     operator fun Array<Cookie>?.get(name: String): String? = this?.firstOrNull { it.name == name }?.value
+    
+    fun cookieStringToMap(cookieString: String): Map<String, String> {
+        val map = HashMap<String, String>()
+        cookieString.split("; ").forEach {
+            val separateIndex = it.indexOf("=")
+            if(separateIndex < 0) return@forEach
+            val key = it.substring(0, separateIndex)
+            val value = if(separateIndex < it.length - 1) {
+                it.substring(separateIndex + 1)
+            } else ""
+            map[key] = value
+        }
+        return map
+    }
 }
